5eb85896cb307c5167a91349b3e43f433c670615
sonotype <contact.marco.accardi@gmail.com>
2022-10-03 11:19:31 +0200

    add remove item and style

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,11 +1,18 @@
 import { Todo } from "../models/todo";
 import { TodoItem } from "./TodoItem";
 
-const Todos: React.FC<{ items: Todo[] }> = ({ items }) => {
+const Todos: React.FC<{
+  items: Todo[];
+  onRemoveTodo: (todoId: string) => void;
+}> = ({ items, onRemoveTodo }) => {
   return (
     <ul className='list-none my-2 mx-auto p-0 w-[40rem]'>
       {items.map((item: Todo) => (
-        <TodoItem key={item.id} text={item.text} />
+        <TodoItem
+          key={item.id}
+          text={item.text}
+          onRemoveTodo={onRemoveTodo.bind(null, item.id)}
+        />
       ))}
     </ul>
   );
